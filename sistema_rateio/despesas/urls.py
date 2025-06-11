661461eb585fbc4a74de5abe54be3fdcda922cec
Egewarth05 <jean.egewarth@hotmail.com>
2025-06-11 17:07:35 -0300

    Update urls.py

--- sistema_rateio/despesas/urls.py
+++ sistema_rateio/despesas/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import nova_despesa, lista_despesas, ver_rateio, excluir_despesa, editar_rateio, ajax_ultima_agua, limpar_rateio, limpar_tudo
+from .views import nova_despesa, lista_despesas, ver_rateio, excluir_despesa, editar_despesa, editar_rateio, ajax_ultima_agua, limpar_rateio, limpar_tudo
 
 urlpatterns = [
     path('', lista_despesas, name='lista_despesas'),
@@ -8,6 +8,7 @@ urlpatterns = [
     path('limpar_rateio/<int:despesa_id>/', limpar_rateio, name='limpar_rateio'),
     path('limpar_tudo/', limpar_tudo, name='limpar_tudo'),
     path('excluir/<int:despesa_id>/', excluir_despesa, name='excluir_despesa'),
+    path('editar/<int:despesa_id>/', editar_despesa, name='editar_despesa'),
     path('editar_rateio/<int:rateio_id>/', editar_rateio, name='editar_rateio'),
     path('ajax/ultima_agua/', ajax_ultima_agua, name='ajax_ultima_agua'),
 
