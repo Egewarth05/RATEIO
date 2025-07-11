7445f14d300699f2c8eb8e3b0047b8527316be0c
Egewarth05 <jean.egewarth@hotmail.com>
2025-07-11 15:13:27 -0300

    Update urls.py

--- sistema_rateio/sistema_rateio/urls.py
+++ sistema_rateio/sistema_rateio/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/despesas/')),
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', RedirectView.as_view(url='/despesas/')),
     path('despesas/', include('despesas.urls')),
 ]
