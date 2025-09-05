25c124dcbb4c234607ef84e51f5ee2cf3f8ee005
Egewarth05 <jean.egewarth@hotmail.com>
2025-09-05 09:48:57 -0300

    Usa favicon pela raiz

--- sistema_rateio/sistema_rateio/urls.py
+++ sistema_rateio/sistema_rateio/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 from . import views
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico'))),
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
